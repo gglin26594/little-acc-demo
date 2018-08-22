f911f0929809ee5dbf625f9130b948554a5d131d
gglin26594 <lin.cityu@gmail.com>
2018-08-22 18:18:28 +1200

    create record table from mock data

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
+import Record from './Record';
 
 
 class Records extends Component {
-  render() {
-    return (
-      <div>
-         <h2>Records</h2>
-      
-	      <table className="table table-bordered">
-	      	<thead> 
-	      		<tr><td>Date</td>
-	      			<td>Title</td>
-	      			<td>Amount</td></tr>
-	      	</thead>
-	      	<tbody>
-	      		<tr>
-	      			<td>2018-01-10</td>
-	      			<td>Income</td>
-	      			<td>25</td>
-	      		</tr>
+	constructor() {
+		super();
+		this.state = {
+			records: [
+				{"id":1, "date": "2018-01-08", "title": "income", "amount": 15},
+				{"id":2, "date": "2018-01-18", "title": "salary", "amount": 1522}
+			]
+			
+		}
+	};
 
-	      	</tbody>
-	      </table>
-      </div>
-    );
-  }
-}
+	  render() {
+	    return (
+	      <div>
+	         <h2>Records</h2>
+	      
+		      <table className="table table-bordered">
+		      	<thead> 
+		      		<tr><td>Date</td>
+		      			<td>Title</td>
+		      			<td>Amount</td>
+		      		</tr>
+		      	</thead>
+		      	<tbody>
+		      		{this.state.records.map((record) => <Record record={record} />)}
+		      		
+		      	</tbody>
+		      </table>
+	      </div>
+	    );
+	  }
+	}
+
+
 
 export default Records;
