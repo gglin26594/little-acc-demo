51346b7db7200349190aa04bf64c39041c4d3d1e
gglin26594 <lin.cityu@gmail.com>
2018-08-23 15:25:27 +1200

    add onChange func to input box and disabled submit button no all input

--- src/components/RecordForm.js
+++ src/components/RecordForm.js
@@ -1,35 +1,63 @@
 import React, { Component } from "react";
 
 export default class RecordForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "",
+      title: "",
+      amount: ""
+    };
+  }
+
+  valid() {
+    return this.state.date && this.state.title && this.state.amount;
+  }
+
+  handleChange(event) {
+    let name, obj;
+    name = event.target.name;
+    this.setState((obj = {}, obj["" + name] = event.target.value, obj));
   }
 
   render() {
     return (
       <div>
-        <form className="form-inline">
+        <form className="form-inline  mb-3">
           <div className="form-group">
             <input
               type="text"
-              className="form-control"
+              className="form-control mr-1"
               placeholder="Date"
               name="date"
+              value={this.state.date}
+              onChange={this.handleChange.bind(this)}
             />
             <input
               type="text"
-              className="form-control"
+              className="form-control  mr-1"
               placeholder="Title"
               name="title"
+              value={this.state.title}
+              onChange={this.handleChange.bind(this)}
             />
             <input
               type="text"
-              className="form-control"
+              className="form-control  mr-1"
               placeholder="Amount"
-              name="Amount"
+              name="amount"
+              value={this.state.amount}
+              onChange={this.handleChange.bind(this)}
             />
           </div>
-          <button type="submit" className="btn btn-primary"> Create Record</button>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={!this.valid()}
+          >
+            
+            Create Record
+          </button>
         </form>
       </div>
     );
